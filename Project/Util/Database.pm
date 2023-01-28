76f0d48ad0489507b8ba76117c71b62a0bcf431b
duofor <marius.cosma95@gmail.com>
2023-01-28 21:03:07 +0200

    Added initial query for each endpoint

--- Project/Util/Database.pm
+++ Project/Util/Database.pm
@@ -4,6 +4,7 @@ use Moose;
 use DBI;
 
 use Data::Dumper;
+use DateTime;
 
 has 'dbh' => (
     is => 'ro',
@@ -48,27 +49,108 @@ sub exec_select {
 }
 
 sub total_duration_of_calls_in_period {
-    my ( $self, $params, $start_date, $end_date ) = @_;
+    my ( $self, $params ) = @_;
 
-    # call_date => 16/08/2016
-    # duration => 43
+    my @where_clause = ( "AND CALL_DATE BETWEEN (?, ?)" );
+    my @query_params = ( $params->{start_date}, $params->{end_date} );
 
-    my $sql = qq { 
-        SELECT *
-        FROM cdr
-        WHERE CALL_DATE between ? and ? 
-    };
-    
+    if ( defined $params->{type} ) {
+        push @where_clause, "AND TYPE = ?";
+        push @query_params, $params->{type}; 
+    }
+
+    my $sql = "SELECT duration " .
+        "FROM cdr " .
+        "WHERE 1 = 1 " .
+        join "\n", @where_clause;
+
     my $sth = $self->dbh->prepare($sql);
-    $sth->execute(@$params);
+    $sth->execute(@query_params);
 
-    my $stopper = 1;
+    my @result;
     while ( my $row = $sth->fetchrow_hashref ) {
-        return $row;
+        next unless $row->{duration};
+        next unless $row->{duration} =~ /^\d+$/;
+
+        push @result, $row->{duration};
     }
 
     $sth->finish();
+
+    return \@result;
 }
 
+sub caller_id {
+    my ( $self, $params ) = @_;
+
+    my @where_clause = ( "AND CALL_DATE BETWEEN (?, ?)" );
+    my @query_params = ( $params->{start_date}, $params->{end_date} );
+
+    push @where_clause, "AND CALLER_ID = ?";
+    push @query_params, $params->{caller_id};
+
+    if ( defined $params->{type} ) {
+        push @where_clause, "AND TYPE = ?";
+        push @query_params, $params->{type}; 
+    } 
+
+    my $sql = "SELECT * " .
+        "FROM cdr " .
+        "WHERE 1 = 1 " .
+        join "\n", @where_clause;
+
+    my $sth = $self->dbh->prepare($sql);
+    $sth->execute(@query_params);
+
+    my @result;
+    while ( my $row = $sth->fetchrow_hashref ) {
+        push @result, $row;
+    }
+
+    $sth->finish();
+
+    return \@result;
+}
+
+sub most_expensive_calls {
+    my ( $self, $params ) = @_;
+
+    my @where_clause = ( "AND CALL_DATE BETWEEN (?, ?)" );
+    my @query_params = ( $params->{start_date}, $params->{end_date} );
+
+    push @where_clause, "AND CALLER_ID = ?";
+    push @query_params, $params->{caller_id};
+
+    if ( defined $params->{type} ) {
+        push @where_clause, "AND TYPE = ?";
+        push @query_params, $params->{type}; 
+    } 
+
+    my $sql = "SELECT * " .
+        "FROM cdr " .
+        "WHERE 1 = 1 " .
+        join "\n", @where_clause, .
+        " ORDER BY cost ASC";
+
+    print $sql;
+    print join ',', @query_params;
+
+    my $sth = $self->dbh->prepare($sql);
+    $sth->execute(@query_params);
+
+    my @result;
+    my $rownum = $params->{number_of_calls};
+    my $counter = 0;
+    while ( my $row = $sth->fetchrow_hashref ) {
+        last if $counter >= $rownum;
+        $counter++;
+
+        push @result, $row;
+    }
+
+    $sth->finish();
+
+    return \@result;
+}
 
 1;
