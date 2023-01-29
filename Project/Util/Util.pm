f139e00e06c2bddf135e33728bb8d8bc263a3718
duofor <marius.cosma95@gmail.com>
2023-01-29 15:54:27 +0200

    Added Util::Util package

---
+++ Project/Util/Util.pm
@@ -0,0 +1,36 @@
+package Util::Util;
+
+use strict;
+use warnings;
+
+use DateTime;
+
+sub is_time_interval_too_large {
+    my ($start_date, $end_date) = @_;
+
+    # this check should really be done in FE
+
+    $start_date =~ /(?<day_start>\d+)\/(?<month_start>\d+)\/(?<year_start>\d+)/;
+    my $start_obj = DateTime->new(
+        day => $+{day_start},
+        month => $+{month_start},
+        year => $+{year_start}
+    );
+
+    $end_date =~ /(?<day_end>\d+)\/(?<month_end>\d+)\/(?<year_end>\d+)/;
+    my $end_obj = DateTime->new(
+        day => $+{day_end},
+        month => $+{month_end},
+        year => $+{year_end}
+    );
+
+    my $duration = $end_obj - $start_obj;
+
+    if ( $duration->{months} > 0 ) {
+        return 1;
+    }
+
+    return 0;
+}
+
+1;
