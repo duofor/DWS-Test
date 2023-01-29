b0c96e582cbe23a829ca215469766f4aa88a25aa
duofor <marius.cosma95@gmail.com>
2023-01-29 16:22:26 +0200

    Added pod for RestApi

--- Project/Test/RestApi.pm
+++ Project/Test/RestApi.pm
@@ -14,4 +14,31 @@ has 'user_agent' => (
     }
 );
 
+=pod 
+
+=head1 NAME
+
+Test::RestApi
+
+=head1 DESCRIPTION
+
+Provides Rest Api tools
+
+=head1 Attributes
+
+=item * user_agent
+
+Attribute holding the user agent.
+Can be extended to use different user agents  
+
+=over 4
+
+=head1 Methods
+
+=over 4
+
+=back
+
+=cut
+
 1;
